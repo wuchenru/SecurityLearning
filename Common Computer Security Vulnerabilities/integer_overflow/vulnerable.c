4f50dfcce53be8a34892ee4a5e66ccba2f68c555
wuchenru <chenruwu31@gmail.com>
2024-11-28 23:22:19 -0800

    update the comments

--- Common Computer Security Vulnerabilities/integer_overflow/vulnerable.c
+++ Common Computer Security Vulnerabilities/integer_overflow/vulnerable.c
@@ -5,6 +5,8 @@ void vulnerable(int len, char *data) {
     memcpy(buf, data, len);
 }
 
+// imagine the attacker can exploit the input arguments like len, and data
+
 // memcpy(void *s1, const void *s2, size_t n);
 
 // size_t is unsigned, int is signed
